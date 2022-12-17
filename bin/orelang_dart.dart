36fe4f6ee77f9ff60148b75b33ea98f717fb5ca1
Uchijo <kgtnttki.yuta@gmail.com>
2022-12-17 19:01:35 +0900

    initial commit

---
+++ bin/orelang_dart.dart
@@ -0,0 +1,5 @@
+import 'package:orelang_dart/orelang_dart.dart' as orelang_dart;
+
+void main(List<String> arguments) {
+  print('Hello, world!');
+}
---
+++ test/orelang_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:orelang_dart/orelang_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
